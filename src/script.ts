5903dcf635c85e2e3dea08ccbe9144b46e40f9f5
satish-313 <pradhansatish53@gmail.com>
2024-01-06 23:31:51 +0530

    made onselect show bold,italic with css

--- src/script.ts
+++ src/script.ts
@@ -4,19 +4,53 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
-let form = document.getElementById("idform")!;
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
+let textChange = document.getElementById("dbtn");
+let tcChild = textChange?.children!;
+for (let i = 0; i < tcChild.length; i++) {
+    let btn = tcChild[i] as HTMLElement;
+    btn.addEventListener("click", function () {
+        let selection = window.getSelection();
+        if (selection === null) return;
+        let tg = btn.dataset.tagtype;
+        if (tg === undefined) return;
+
+        let editableParagraph = selection.anchorNode;
+        if (editableParagraph === null) return;
+
+        if (
+            selection.rangeCount > 0 &&
+            editableParagraph.contains(selection.anchorNode)
+        ) {
+            let range = selection.getRangeAt(0);
+            let changeEl = document.createElement(tg);
+            changeEl.appendChild(range.extractContents());
+            range.insertNode(changeEl);
+        }
+    });
+}
+let lyover = document.getElementById("layover");
+lyover?.addEventListener("click", function () {
+    this.style.display = "none";
+    if (textChange) {
+        textChange.style.display = "none";
+    }
 });
 
 let editor = document.getElementById("editor-fields")!;
 editor.addEventListener("keydown", function (e) {
     if (e.key === "Enter") {
         e.preventDefault();
-        const curEle = document.activeElement;
+        const curEle = document.activeElement as HTMLElement | null;
         if (curEle === null) return;
         const parEle = curEle.parentNode as HTMLElement;
-        addInput(parEle);
+        let selection = window.getSelection();
+        if (!selection) return;
+        let cursorPosition = selection.focusOffset;
+        console.log(curEle.textContent);
+        let textBeforeCursor = curEle.textContent!.substring(0, cursorPosition);
+        let textAfterCursor = curEle.textContent!.substring(cursorPosition);
+        curEle.innerHTML = textBeforeCursor;
+        addInput(parEle, textAfterCursor);
         return;
     }
 
@@ -44,20 +78,45 @@ editor.addEventListener("keydown", function (e) {
             e.preventDefault();
             prevp.focus();
         }
+        return;
     }
 });
 
-function addInput(parent?: null | HTMLElement) {
+function addInput(parent?: null | HTMLElement, value?: undefined | string) {
     const div = document.createElement("div");
     const p = document.createElement("p");
     p.setAttribute("contenteditable", "true");
-    div.appendChild(p);
     p.addEventListener("mouseup", function () {
-        
+        let selection = window.getSelection();
+        if (selection === null) return;
+        let sText = selection.toString();
+
+        if (sText.length > 1 && textChange && lyover) {
+            let range = selection.getRangeAt(0);
+            let clientRect = range.getBoundingClientRect();
+            console.log("clientRect", clientRect);
+            textChange.style.display = "flex";
+            textChange.style.position = "absolute";
+            textChange.style.top = `${clientRect.y}px`;
+            textChange.style.left = `${
+                (clientRect.left + clientRect.right) / 2
+            }px`;
+            lyover.style.display = "block";
+        }
     });
+    div.appendChild(p);
 
     if (parent != null) {
-        parent.insertAdjacentElement("afterend", div);
-    } else editor.appendChild(div);
+        if (!value) {
+            parent.insertAdjacentElement("afterend", div);
+        } else {
+            p.innerText = value;
+            parent.insertAdjacentElement("afterend", div);
+        }
+    } else {
+        p.innerText =
+            "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to s have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
+        editor.appendChild(div);
+    }
     p.focus();
 }
