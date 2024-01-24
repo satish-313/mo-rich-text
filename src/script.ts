34bbfcead297c0b6430fd7161d6795654604309f
satish-313 <pradhansatish53@gmail.com>
2024-01-24 20:39:40 +0530

    finally debug caret problem

--- src/script.ts
+++ src/script.ts
@@ -4,6 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
+type MyNode = Node & {
+    length : number;
+};
+
 let textChange = document.getElementById("dbtn");
 let tcChild = textChange?.children!;
 for (let i = 0; i < tcChild.length; i++) {
@@ -32,55 +36,12 @@ let lyover = document.getElementById("layover");
 lyover?.addEventListener("click", function () {
     this.style.display = "none";
     if (textChange) {
+        window.getSelection()?.removeAllRanges();
         textChange.style.display = "none";
     }
 });
 
 let editor = document.getElementById("editor-fields")!;
-editor.addEventListener("keydown", function (e) {
-    if (e.key === "Enter") {
-        e.preventDefault();
-        const curEle = document.activeElement as HTMLElement | null;
-        if (curEle === null) return;
-        const parEle = curEle.parentNode as HTMLElement;
-        let selection = window.getSelection();
-        if (!selection) return;
-        let cursorPosition = selection.focusOffset;
-        console.log(curEle.textContent);
-        let textBeforeCursor = curEle.textContent!.substring(0, cursorPosition);
-        let textAfterCursor = curEle.textContent!.substring(cursorPosition);
-        curEle.innerHTML = textBeforeCursor;
-        addInput(parEle, textAfterCursor);
-        return;
-    }
-
-    if (e.key === "Backspace") {
-        const curEle = document.activeElement as HTMLParagraphElement;
-        if (curEle === null) return;
-        const parEle = curEle.parentNode as HTMLElement;
-        const prevEle = parEle.previousElementSibling as HTMLElement | null;
-        if (prevEle === null) return;
-        const prevp = prevEle.querySelector("p");
-        if (prevp === null) return;
-        if (
-            editor.firstElementChild != parEle &&
-            curEle.innerText.length <= 0
-        ) {
-            editor.removeChild(parEle);
-            let range = document.createRange();
-            range.selectNodeContents(prevp);
-            range.collapse(false);
-
-            let selection = window.getSelection();
-            if (selection === null) return;
-            selection.removeAllRanges();
-            selection.addRange(range);
-            e.preventDefault();
-            prevp.focus();
-        }
-        return;
-    }
-});
 
 function addInput(parent?: null | HTMLElement, value?: undefined | string) {
     const div = document.createElement("div");
@@ -94,7 +55,6 @@ function addInput(parent?: null | HTMLElement, value?: undefined | string) {
         if (sText.length > 1 && textChange && lyover) {
             let range = selection.getRangeAt(0);
             let clientRect = range.getBoundingClientRect();
-            console.log("clientRect", clientRect);
             textChange.style.display = "flex";
             textChange.style.position = "absolute";
             textChange.style.top = `${clientRect.y}px`;
@@ -104,6 +64,56 @@ function addInput(parent?: null | HTMLElement, value?: undefined | string) {
             lyover.style.display = "block";
         }
     });
+    p.addEventListener("keydown", function (e) {
+        if (e.key === "Enter") {
+            e.preventDefault();
+            const parEle = this.parentNode as HTMLElement;
+            let selection = document.getSelection();
+            if (!selection) return;
+            let cursorPosition = selection.focusOffset;
+            console.log("cursorPosition", cursorPosition);
+            let textBeforeCursor = this.textContent!.substring(
+                0,
+                cursorPosition
+            );
+            let textAfterCursor = this.textContent!.substring(cursorPosition);
+            this.innerText = textBeforeCursor;
+            addInput(parEle, textAfterCursor);
+            return;
+        }
+
+        if (e.key === "Backspace") {
+            const parEle = this.parentNode as HTMLElement;
+            const prevEle = parEle.previousElementSibling as HTMLElement | null;
+            if (prevEle === null) return;
+            const prevp = prevEle.querySelector("p")!;
+            let textNodes = getTextNodes(prevp);
+
+            if (prevp === null) return;
+            if (
+                editor.firstElementChild != parEle &&
+                this.innerText.length <= 0
+            ) {
+                e.preventDefault();
+                var lastTextNode = textNodes[textNodes.length - 1] as MyNode;
+                editor.removeChild(parEle);
+                let range = document.createRange();
+                range.setStart(lastTextNode, lastTextNode.length);
+                range.collapse(true);
+                console.log(range);
+                let sele = document.getSelection();
+                sele?.removeAllRanges();
+                sele?.addRange(range);
+                prevp.focus();
+            }
+            return;
+        }
+    });
+
+    let toggle = document.createElement("span");
+    toggle.setAttribute("class", "toogle");
+    toggle.innerText = "+";
+    div.appendChild(toggle);
     div.appendChild(p);
 
     if (parent != null) {
@@ -115,8 +125,19 @@ function addInput(parent?: null | HTMLElement, value?: undefined | string) {
         }
     } else {
         p.innerText =
-            "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to s have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
+            "It is a long established fact t of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to s ";
         editor.appendChild(div);
     }
     p.focus();
+}
+
+function getTextNodes(node: HTMLParagraphElement) {
+    var textNodes = [];
+    var walker = document.createTreeWalker(node, NodeFilter.SHOW_TEXT, null);
+
+    while (walker.nextNode()) {
+        textNodes.push(walker.currentNode);
+    }
+
+    return textNodes;
 }
