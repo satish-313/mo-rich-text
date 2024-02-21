47cf44287d8b85d78f4b28e3c242ea12bac71e87
satish-313 <pradhansatish53@gmail.com>
2024-02-21 19:03:07 +0530

    icons added

--- src/script.ts
+++ src/script.ts
@@ -4,9 +4,11 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
+let isRotate = false;
 type MyNode = Node & {
     length: number;
 };
+const imgs = ["photo", "h3", "h4", "list", "code", "embed", "line"];
 
 let textChange = document.getElementById("dbtn");
 let tcChild = textChange?.children!;
@@ -45,81 +47,38 @@ lyover?.addEventListener("click", function () {
 let editor = document.getElementById("editor-fields")!;
 
 function addInput(parent?: null | HTMLElement, value?: undefined | string) {
-    const div = document.createElement("div");
+    const div = createHTML<HTMLDivElement>(`<div class='section'></div>`);
     const p = document.createElement("p");
+    const toggle = createHTML<HTMLSpanElement>(
+        `<span class='toggle'><span>+</span></span>`
+    );
+    const plus = toggle.querySelector("span")!;
     p.setAttribute("contenteditable", "true");
-
-    p.addEventListener("mouseup", function () {
-        let selection = window.getSelection();
-        if (selection === null) return;
-        let sText = selection.toString();
-
-        if (sText.length > 1 && textChange && lyover) {
-            let range = selection.getRangeAt(0);
-            let clientRect = range.getBoundingClientRect();
-            textChange.style.display = "flex";
-            textChange.style.position = "absolute";
-            textChange.style.top = `${clientRect.y}px`;
-            textChange.style.left = `${
-                (clientRect.left + clientRect.right) / 2
-            }px`;
-            lyover.style.display = "block";
+    p.addEventListener("mouseup", () => mouseUp());
+    p.addEventListener("keydown", (e) =>
+        keyDown<HTMLParagraphElement, KeyboardEvent>(p, e)
+    );
+    p.addEventListener("input", (e) => {
+        if (p.innerText.length === 0) {
+            plus.style.display = "flex";
+        } else {
+            plus.style.display = "none";
         }
     });
-
-    p.addEventListener("keydown", function (e) {
-        if (e.key === "Enter") {
-            e.preventDefault();
-            const parEle = this.parentNode as HTMLElement;
-            let cursorPosition = getCaretCharacterOffsetWithin(this);
-
-            let textBeforeCursor = this.textContent!.substring(
-                0,
-                cursorPosition
-            );
-            let textAfterCursor = this.textContent!.substring(cursorPosition);
-            this.innerHTML = textBeforeCursor;
-            addInput(parEle, textAfterCursor);
-            return;
-        }
-
-        if (e.key === "Backspace") {
-            const parEle = this.parentNode as HTMLElement;
-            const prevEle = parEle.previousElementSibling as HTMLElement | null;
-            if (prevEle === null) return;
-            const prevp = prevEle.querySelector("p")!;
-
-            if (
-                editor.firstElementChild != parEle &&
-                (this.innerText.length <= 0 ||
-                    (this.innerText.trim().length === 0 &&
-                        this.innerText.length === 1))
-            ) {
-                e.preventDefault();
-                if (prevp.innerText.length !== 0) {
-                    let textNodes = document
-                        .createTreeWalker(prevp, NodeFilter.SHOW_TEXT, null)
-                        .nextNode() as MyNode;
-
-                    let range = document.createRange();
-                    range.setStart(textNodes, textNodes.length);
-                    range.collapse(true);
-                    let sele = document.getSelection();
-                    sele?.removeAllRanges();
-                    sele?.addRange(range);
-                }
-                editor.removeChild(parEle);
-                prevp.focus();
-            }
-            return;
+    p.addEventListener("focusin", () => {
+        if (p.innerText.length === 0) {
+            plus.style.display = "flex";
+        } else {
+            plus.style.display = "none";
         }
     });
+    p.addEventListener("focusout", () => (plus.style.display = "none"));
 
-    let toggle = document.createElement("span");
-    toggle.setAttribute("class", "toogle");
-    toggle.innerText = "+";
+    plus.addEventListener("click", () => showRadioType(toggle));
+
     div.appendChild(toggle);
     div.appendChild(p);
+    createHtmltype<HTMLDivElement>(div);
 
     if (parent != null) {
         if (!value) {
@@ -131,8 +90,10 @@ function addInput(parent?: null | HTMLElement, value?: undefined | string) {
     } else {
         editor.appendChild(div);
     }
+
     p.focus();
 }
+
 function getCaretCharacterOffsetWithin(element: Node) {
     let caretOffset = 0;
     let sel;
@@ -151,4 +112,98 @@ function getCaretCharacterOffsetWithin(element: Node) {
     }
 
     return caretOffset;
+}
+
+function createHtmltype<T extends HTMLElement>(parent: T) {
+    const div = createHTML<HTMLDivElement>(`<div class="radio"></div>`);
+    for (let r of imgs) {
+        let temp =
+            createHTML(`<div><input type="radio" id=${r} name="html-type" value=${r} />
+    <label for=${r} data-alt="add ${r}"></label></div>`);
+        let lable = temp.querySelector("label")!;
+        lable.style.backgroundImage = `url(./img/${r}.svg)`;
+        div.appendChild(temp);
+    }
+    parent.appendChild(div);
+}
+
+function createHTML<T extends HTMLElement>(htmlString: string) {
+    const template = document.createElement("template");
+    template.innerHTML = htmlString.trim();
+    return template.content.firstChild as T;
+}
+
+function showRadioType<T extends HTMLElement>(target: T) {
+    let span = target.querySelector("span");
+    if (!span) return;
+    if (isRotate) {
+        span.style.transform = `rotate(0deg)`;
+    } else {
+        span.style.transform = `rotate(45deg)`;
+    }
+    isRotate = !isRotate;
+}
+
+function mouseUp() {
+    let selection = window.getSelection();
+    if (selection === null) return;
+    let sText = selection.toString();
+
+    if (sText.length > 1 && textChange && lyover) {
+        let range = selection.getRangeAt(0);
+        let clientRect = range.getBoundingClientRect();
+        textChange.style.display = "flex";
+        textChange.style.position = "absolute";
+        textChange.style.top = `${clientRect.y}px`;
+        textChange.style.left = `${(clientRect.left + clientRect.right) / 2}px`;
+        lyover.style.display = "block";
+    }
+}
+
+function keyDown<H extends HTMLElement, E extends KeyboardEvent>(
+    target: H,
+    e: E
+) {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        const parEle = target.parentNode as HTMLElement;
+        let cursorPosition = getCaretCharacterOffsetWithin(target);
+
+        let textBeforeCursor = target.textContent!.substring(0, cursorPosition);
+        let textAfterCursor = target.textContent!.substring(cursorPosition);
+        target.innerHTML = textBeforeCursor;
+        addInput(parEle, textAfterCursor);
+        return;
+    }
+
+    if (e.key === "Backspace") {
+        const parEle = target.parentNode as HTMLElement;
+        const prevEle = parEle.previousElementSibling as HTMLElement | null;
+        if (prevEle === null) return;
+        const prevp = prevEle.querySelector("p")!;
+
+        if (
+            editor.firstElementChild != parEle &&
+            (target.innerText.length <= 0 ||
+                (target.innerText.trim().length === 0 &&
+                    target.innerText.length === 1))
+        ) {
+            e.preventDefault();
+            if (prevp.innerText.length !== 0) {
+                let textNodes = document
+                    .createTreeWalker(prevp, NodeFilter.SHOW_TEXT, null)
+                    .nextNode() as MyNode;
+
+                let range = document.createRange();
+                range.setStart(textNodes, textNodes.length);
+                range.collapse(true);
+                let sele = document.getSelection();
+                sele?.removeAllRanges();
+                sele?.addRange(range);
+            }
+            editor.removeChild(parEle);
+            prevp.focus();
+        }
+        return;
+    }
 }
